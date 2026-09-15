632e56f192e419000794dfc41ca50f4f9c802bf6
quangnv1911 <quangnv1911@gmail.com>
2026-09-15 14:26:45 +0700

    feat: update gateway for fintrack webhook

--- src/main/java/com/quangnv/service/gateway/config/GatewayConfig.java
+++ src/main/java/com/quangnv/service/gateway/config/GatewayConfig.java
@@ -50,9 +50,7 @@ public class GatewayConfig {
         builder.route("public-fin-track", r -> r.path(
                         CommonConstant.API_PREFIX + RouteNameConstant.FINANCE_TRACKING + "/public-webhook/**"
                 )
-                .filters(f -> f
-                        .stripPrefix(2)
-                        .filter(apiKeyAuthFilter))
+                .filters(f -> f.stripPrefix(3))
                 .uri(ServiceConstant.ServiceName.FIN_TRACK_SERVICE.toLoadBalancedUri()));
     }
 
